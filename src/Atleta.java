34b0e15c0556c18d18fbe6cf4ec85dd4f9a3f0c5
Renan Mendonça Lopes <37083197+RenanMLopes@users.noreply.github.com>
2024-07-17 22:04:01 -0300

    Exercicio 7 resolução

--- src/Atleta.java
+++ src/Atleta.java
@@ -4,7 +4,7 @@ public class Atleta {
     public static void main(String[] args) {
         int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade do atleta"));
 
-        String categoria = (idade < 18) ? "Juvenil" : (idade < 40) ? "Adulto" : "Master";
+        String categoria = (idade < 18) ? "Juvenil" : (idade <= 40) ? "Adulto" : "Master";
         System.out.println("o Atleta esta na categoria: " + categoria);
 
     }
