6ebff34878efb3ae7c870ad0cd2b82db7a1f62bb
Renan Mendonça Lopes <37083197+RenanMLopes@users.noreply.github.com>
2024-07-17 21:15:14 -0300

    Exercicio 6 resolução

--- src/NumeroMaior.java
+++ src/NumeroMaior.java
@@ -2,18 +2,18 @@ import javax.swing.*;
 
 public class NumeroMaior {
     public static void main(String[] args) {
-        int a = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero"));
-        int b = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero"));
-        int c = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro numero"));
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero"));
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero"));
+        int num3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro numero"));
 
         int x = 0;
 
-        if(a > b && a > c){
-            x = a;
-        }else if(b > a && b > c){
-            x = b;
+        if(num1 > num2 && num1 > num3){
+            x = num1;
+        }else if(num2 > num1 && num2 > num3){
+            x = num2;
         }else
-            x = c;
+            x = num3;
 
         System.out.println("o maior número é:" + x);
     }
